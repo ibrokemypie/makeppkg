062cf58d560649ce41335cb69baff47483415a83
Conor Randall <ibrokemypie@bastardi.net>
2018-10-02 22:29:47 +0930

    Skip makeppkg if non-building args detected

--- src/arg_parse.rs
+++ src/arg_parse.rs
@@ -4,7 +4,7 @@ use std::env::args;
 use std::path::PathBuf;
 
 // Parses arguments from CLI
-pub fn arg_parse() -> (Vec<String>, PathBuf, String) {
+pub fn arg_parse() -> (Vec<String>, Option<(PathBuf, String)>) {
     // Get fallback XDG config home
     let xdg_dirs = xdg::BaseDirectories::with_prefix("makeppkg").unwrap();
     let xdghome = xdg_dirs
@@ -15,27 +15,41 @@ pub fn arg_parse() -> (Vec<String>, PathBuf, String) {
     let mut location = PathBuf::from(xdghome);
     let mut pkgbuild_path = "PKGBUILD".to_string();
 
+    let blacklist = vec![
+        "noextract",
+        "verifysource",
+        "geninteg",
+        "repackage",
+        "source",
+        "version",
+        "allsource",
+        "packagelist",
+        "printsrcinfo",
+    ];
+
     let mut options: Vec<String> = args().collect();
+    options.remove(0);
 
     let iter = options.to_owned();
     let mut iter = iter.iter().enumerate().peekable();
     while let Some((i, value)) = iter.next() {
+        if blacklist.contains(&value.trim_left_matches("-")) {
+            return (options, None);
+        }
         if &value == &"-l" {
-            //let arg = iter.peek();
             if iter.peek().is_some() {
                 location = PathBuf::from(iter.peek().unwrap().1);
                 options.remove(i + 1);
                 options.remove(i);
-            } else {}
+            } else {
+            }
         }
         if &value == &"-p" {
-            //let arg = iter.peek();
             if iter.peek().is_some() {
                 pkgbuild_path = iter.peek().unwrap().1.to_string();
             }
         }
     }
-    options.remove(0);
 
-    return (options, location, pkgbuild_path);
+    return (options, Some((location, pkgbuild_path)));
 }
--- src/main.rs
+++ src/main.rs
@@ -15,51 +15,52 @@ use package_name::package_name;
 use patch::patch;
 use std::fs::File;
 
-
 fn main() {
     // Parse CLI args
-    let (options, location, pkgbuild_path) = arg_parse();
+    let (options, other) = arg_parse();
 
-    eprintln!(
-        "makeppkg directory: {}, makepkg arguments: {}, pkgbuild path: {}",
-        location.to_string_lossy(),
-        options.join(" "),
-        pkgbuild_path
-    );
+    if other.is_some() {
+        let (location, pkgbuild_path) = other.unwrap();
+        eprintln!(
+            "makeppkg directory: {}, makepkg arguments: {}, pkgbuild path: {}",
+            location.to_string_lossy(),
+            options.join(" "),
+            pkgbuild_path
+        );
 
-    // Open PKGBUILD and return an error if fails
-    match File::open(pkgbuild_path).map_err(|e| e.to_string()) {
-        Ok(_) => {
-            match cmd("makepkg", vec!["--printsrcinfo"])
-                .stderr_to_stdout()
-                .read()
-            {
-                Ok(srcinfo) => {
-                    match package_name(&srcinfo) {
-                        Ok(pkgname) => {
-                            eprintln!("Package name: {}", pkgname);
-                            match patch(location, pkgname, &srcinfo) {
-                                Ok(_) => {}
-                                Err(error) => {
-                                    eprintln!("Could not run patches, continuing: {:?}", error)
+        // Open PKGBUILD and return an error if fails
+        match File::open(pkgbuild_path).map_err(|e| e.to_string()) {
+            Ok(_) => {
+                match cmd("makepkg", vec!["--printsrcinfo"])
+                    .stderr_to_stdout()
+                    .read()
+                {
+                    Ok(srcinfo) => {
+                        match package_name(&srcinfo) {
+                            Ok(pkgname) => {
+                                eprintln!("Package name: {}", pkgname);
+                                match patch(location, pkgname, &srcinfo) {
+                                    Ok(_) => {}
+                                    Err(error) => {
+                                        eprintln!("Could not run patches, continuing: {:?}", error)
+                                    }
                                 }
                             }
-                        }
-                        Err(error) => {
-                            eprintln!("Could not retireve package name, continuing: {:?}", error)
-                        }
-                    };
-                }
-                Err(error) => eprintln!("Failed run makepkg --printsrcinfo: {:?}", error),
-            };
-        }
-        Err(error) => eprintln!("Couldn't open PKGBUILD: {}", error),
-    };
+                            Err(error) => eprintln!(
+                                "Could not retireve package name, continuing: {:?}",
+                                error
+                            ),
+                        };
+                    }
+                    Err(error) => eprintln!("Failed run makepkg --printsrcinfo: {:?}", error),
+                };
+            }
+            Err(error) => eprintln!("Couldn't open PKGBUILD: {}", error),
+        };
+    }
+
     // Run makepkg
-    match cmd("makepkg", options)
-        .stderr_to_stdout()
-        .run()
-    {
+    match cmd("makepkg", options).stderr_to_stdout().run() {
         Ok(_) => {}
         Err(error) => eprintln!("Failed to run makepkg: {}", error),
     };
